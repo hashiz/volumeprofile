3c50ee122b74cbca1ae71addded414aac34ff974
hashiz <hashiz@meridiani.jp>
2013-09-03 18:54:41 +0900

    * fix NPE on preference change * fix Toast and Vibrate on Profile Applying

--- VolumeProfile/src/jp/meridiani/apps/volumeprofile/prefs/Prefs.java
+++ VolumeProfile/src/jp/meridiani/apps/volumeprofile/prefs/Prefs.java
@@ -10,8 +10,8 @@ import android.preference.PreferenceManager;
 
 public class Prefs implements OnSharedPreferenceChangeListener {
 	private static Prefs mInstance = null;
-	private static final String KEY_VIBRATEONPROFILECHANGE      = "vibrate_on_profile_change";
 	private static final String KEY_DISPLAYTOASTONPROFILECHANGE = "display_toast_on_profile_change";
+	private static final String KEY_VIBRATEONPROFILECHANGE      = "vibrate_on_profile_change";
 	private static final String KEY_PLAYSOUNDONVOLUMECHANGE     = "play_sound_on_volume_change";
 	private static final String KEY_SOUNDLEVELALERTHACK         = "sound_level_alert_hack";
 
@@ -37,18 +37,18 @@ public class Prefs implements OnSharedPreferenceChangeListener {
 		editor.apply();
 	}
 
-	public boolean isVibrateOnProfileChange() {
-		return mPrefs.getBoolean(KEY_VIBRATEONPROFILECHANGE, true);
-	}
-	public void setVibrateOnProfileChange(boolean value) {
-		setValue(KEY_VIBRATEONPROFILECHANGE, value);
-	}
-
 	public boolean isDisplayToastOnProfileChange() {
 		return mPrefs.getBoolean(KEY_DISPLAYTOASTONPROFILECHANGE, true);
 	}
 	public void setDisplayToastOnProfileChange(boolean value) {
 		setValue(KEY_DISPLAYTOASTONPROFILECHANGE, value);
+	}
+
+	public boolean isVibrateOnProfileChange() {
+		return mPrefs.getBoolean(KEY_VIBRATEONPROFILECHANGE, true);
+	}
+	public void setVibrateOnProfileChange(boolean value) {
+		setValue(KEY_VIBRATEONPROFILECHANGE, value);
 	}
 
 	public boolean isPlaySoundOnVolumeChange() {
