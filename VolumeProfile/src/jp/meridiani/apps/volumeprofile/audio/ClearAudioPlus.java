2e997423e26a26738cbaa55483c674733c837416
hashiz <hashiz@meridiani.jp>
2015-09-19 14:49:57 +0900

    * ClearAudio+ support on lollipop

--- VolumeProfile/src/jp/meridiani/apps/volumeprofile/audio/ClearAudioPlus.java
+++ VolumeProfile/src/jp/meridiani/apps/volumeprofile/audio/ClearAudioPlus.java
@@ -1,112 +1,94 @@
 package jp.meridiani.apps.volumeprofile.audio;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.media.AudioManager;
 
 public class ClearAudioPlus {
 
-	// for reflection
-	private static final String ClearAudioPlus   = "com.sonymobile.audioeffect.ClearAudioPlus";
-	private static final String isGlobalSetting  = "isGlobalSetting";
-	private static final String getCurrentState = "getCurrentState";
-
+	// for Action Intent
 	private static final String ACTION_CLEARAUDIO_PLUS_STATUS = "com.sonymobile.audioeffect.intent.action.CLEARAUDIO_PLUS_STATUS";
 	private static final String ACTION_CLEARAUDIO_PLUS_REQUEST = "com.sonymobile.audioeffect.intent.action.CLEARAUDIO_PLUS_REQUEST";
     private static final String EXTRA_CLEARAUDIO_PLUS_STATUS = "com.sonymobile.audioeffect.intent.extra.CLEARAUDIO_PLUS_STATUS";
+
+    // for reflection
+    private static final String CLASS_CLEARAUDIOPLUS = "com.sonymobile.audioeffect.ClearAudioPlus";
+    private static final String METHOD_ISGLOBALSETTING = "isGlobalSetting";
+    private static final String METHOD_GETCURRENTSTATE = "getCurrentState";
 
     // old param
 	private static final String AUDIO_PARAM_SUPPORTED_EFFECT = "Sony_effect;supported_effect";
 	private static final String AUDIO_EFFECT_CLEARAUDIO_PLUS = "ClearAudio+";
 	private static final String AUDIO_PARAM_CLEARAUDIO_PLUS_STATE = "Sony_effect;ca_plus_state";
 
-	// new param
-	private static final String CLEAR_AUDIO_PLUS_PREFS = "SE-CLEAR_AUDIO_PLUS";
-	private static final String KEY_CA_PLUS_PREF_STATE = "C_A_PLUS";
-
 	private Context mContext = null;
 	private AudioManager mAmgr = null;
-	private Object mClearAudioPlus = null;
 	private boolean mIsSupported = false;
+	private boolean mIsNewMethod = false;
 
+	private boolean methodInvoke(String className, String methodName) {
+		try {
+			Class<?> c = Class.forName(className);
+			Object instance = c.getConstructor(new Class[] {Context.class}).newInstance(new Object[] {mContext});
+			return ((Boolean)c.getMethod(methodName,new Class [0]).invoke(instance, new Object [0])).booleanValue();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+		catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		catch (InstantiationException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	private void checkSupported() {
+		mIsSupported = false;
+		mIsNewMethod = false;
+		if (methodInvoke(CLASS_CLEARAUDIOPLUS, METHOD_ISGLOBALSETTING)) {
+			mIsSupported = true;
+			mIsNewMethod = true;
+			return;
+		}
+
+		String param = mAmgr.getParameters(AUDIO_PARAM_SUPPORTED_EFFECT);
+		if (param != null && !param.isEmpty()) {
+			String[] keyval = param.split("=");
+			if (keyval.length >= 2) {
+				for (String effect : keyval[1].split(":")) {
+					if (effect.equals(AUDIO_EFFECT_CLEARAUDIO_PLUS)) {
+						mIsSupported = true;
+			    		mIsNewMethod = false;
+			    		return;
+					}
+				}
+			}
+	    }
+	}
 	public ClearAudioPlus(Context context, AudioManager amgr) {
 		mContext = context;
 		mAmgr = amgr;
-		mIsSupported = false;
-	    if (classExists(ClearAudioPlus)) {
-			Object mClearAudioPlus = newInstance(ClearAudioPlus);
-			if (mClearAudioPlus!=null) {
-				mIsSupported = getResultBoolean(mClearAudioPlus,isGlobalSetting);
-			}
-	    }
-	    else {
-			String param = mAmgr.getParameters(AUDIO_PARAM_SUPPORTED_EFFECT);
-			if (param != null && !param.isEmpty()) {
-				String[] keyval = param.split("=");
-				if (keyval.length >= 2) {
-					for (String effect : keyval[1].split(":")) {
-						if (effect.equals(AUDIO_EFFECT_CLEARAUDIO_PLUS)) {
-							mIsSupported = true;
-						}
-					}
-				}
-			}
-			else {
-				param = mAmgr.getParameters(CLEAR_AUDIO_PLUS_PREFS);
-				if (param != null && !param.isEmpty()) {
-					String[] keyval = param.split("=");
-					if (keyval.length >= 2) {
-						for (String effect : keyval[1].split(":")) {
-							if (effect.equals(AUDIO_EFFECT_CLEARAUDIO_PLUS)) {
-								mIsSupported = true;
-							}
-						}
-					}
-				}
-			}
-	    }
+		checkSupported();
 	}
 
-	private boolean classExists(String className) {
-		try {
-	      Class.forName(className);
-	      return true;
-	    }
-	    catch (ClassNotFoundException localClassNotFoundException) {}
-	    return false;
-	}
-
-	private Object newInstance(String className) {
-		Object instance = null;
-	    try {
-		      instance = Class.forName(className).getConstructor(new Class[] { Context.class }).newInstance(new Object[] { mContext });
-	    }
-	    catch (Exception e) {
-	    	return null;
-	    }
-	    return instance;
-	}
-
-	private boolean getResultBoolean(Object instance, String method) {
-		boolean result = false;
-		try {
-	        result = ((Boolean)instance.getClass().getMethod(method, new Class[0]).invoke(instance, new Object[0])).booleanValue();
-		}
-	    catch (Exception e) {
-	    	return false;
-	    }
-	    return result;
-	}
-
-	private boolean useOldMethod() {
-		if (mClearAudioPlus != null) {
-			return false;
-		}
-		String param = mAmgr.getParameters(AUDIO_PARAM_SUPPORTED_EFFECT);
-		if (param != null) {
-			return true;
-		}
-		return false;
+	private boolean useNewMethod() {
+		return mIsNewMethod;
 	}
 
 	public boolean isSupported() {
@@ -117,7 +99,10 @@ public class ClearAudioPlus {
 	    if (!isSupported()) {
 	    	return false;
 	    }
-	    if (useOldMethod()) {
+	    if (useNewMethod()) {
+			return methodInvoke(CLASS_CLEARAUDIOPLUS, METHOD_GETCURRENTSTATE);
+	    }
+	    else {
 			String param = mAmgr.getParameters(AUDIO_PARAM_CLEARAUDIO_PLUS_STATE);
 			if (param == null) {
 				return false;
@@ -135,9 +120,6 @@ public class ClearAudioPlus {
 			}
 			return enabled;
 	    }
-	    else {
-	    	return getResultBoolean(mClearAudioPlus, getCurrentState);
-	    }
 	}
 
 	public void setState(boolean enable) {
@@ -147,11 +129,11 @@ public class ClearAudioPlus {
 
 	    int state = enable ? 1 : 0;
 		Intent intent = null;
-		if (useOldMethod()) {
-			intent = new Intent(ACTION_CLEARAUDIO_PLUS_STATUS);
+		if (useNewMethod()) {
+			intent = new Intent(ACTION_CLEARAUDIO_PLUS_REQUEST);
 		}
 		else {
-			intent = new Intent(ACTION_CLEARAUDIO_PLUS_REQUEST);
+			intent = new Intent(ACTION_CLEARAUDIO_PLUS_STATUS);
 		}
 		intent.putExtra(EXTRA_CLEARAUDIO_PLUS_STATUS, state);
 		mContext.sendBroadcast(intent);
