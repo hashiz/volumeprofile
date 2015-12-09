3e14856f20c7c8c3082d80407b3bc03be9003d33
hashiz <hashiz@meridiani.jp>
2015-12-09 23:57:56 +0900

    * convert android studio

--- VolumeProfile/src/jp/meridiani/apps/volumeprofile/main/ProfileEditCallback.java
+++
@@ -1,8 +0,0 @@
-package jp.meridiani.apps.volumeprofile.main;
-
-import jp.meridiani.apps.volumeprofile.profile.VolumeProfile;
-
-public interface ProfileEditCallback {
-	public void onProfileEditPositive(VolumeProfile newProfile);
-	public void onProfileEditNegative();
-}
